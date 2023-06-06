d911ff6cf26c625371c85db9ef7b985760452dcf
Feyzullah Temel <109575132+FeyzullahTemel0@users.noreply.github.com>
2023-06-06 11:32:46 +0300

    Update day6.py

--- Bulmaca/Day6/day6.py
+++ Bulmaca/Day6/day6.py
@@ -18,15 +18,15 @@ print("İlk paket başlangıcı", result, ". karakterde bulunuyor.")
 
 # Part 2
 
-def find_start_of_message(datastream):
-    with open(datastream, 'r') as file:
-        buffer = file.read().strip()
+def find_start_of_message(ff):
+    with open(ff, 'r') as file:
+        file_buffer = file.read().strip()
 
-    for i in range(13, len(buffer)):
-        if len(set(buffer[i-13:i+1])) == 14:
+    for i in range(13, len(file_buffer)):
+        if len(set(file_buffer[i-13:i+1])) == 14:
             return i + 1
     return -1
 
-datastream_file = "veriler.txt"
-result = find_start_of_message(datastream_file)
+ff_file = "veriler.txt"
+result = find_start_of_message(ff_file)
 print("İlk mesaj başlangıcı", result, ". karakterde bulunuyor.")
